59efcaf8d5ae3838da846340119746b74d1ebe9f
Rusu Dinu <dinustefan89.ds@gmail.com>
2026-02-07 12:23:54 +0200

    Initial commit

---
+++ src/main/java/com/rusudinu/wad_lab3/WadLab3Application.java
@@ -0,0 +1,13 @@
+package com.rusudinu.wad_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadLab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rusudinu/wad_lab3/WadLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusudinu.wad_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
